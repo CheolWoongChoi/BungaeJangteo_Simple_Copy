29f5e85c324a98a0d309891b357b83fd61273fb8
CheolWoong <bluesky4381@naver.com>
2020-12-14 23:19:15 +0900

    ADD: normalize.css, eslint, prettier 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './App.scss';
 
 const App = () => {
-	return (
-		<div className='container'>
-			<div className='img-wrap'>
-				<img src='public/images/img1.jpg' alt='img1' />
-				<img src='public/images/img2.jpg' alt='img2' />
-			</div>
-			<div className='css-wrap'>
-
-			</div>
-		</div>
-	);
-}
+  const fp = 1;
+	
+  return (
+    <div className="container">
+      <div className="img-wrap">
+        <img src="public/images/img1.jpg" alt="img1" />
+        <img src="public/images/img2.jpg" alt="img2" />
+      </div>
+      <div className="css-wrap"></div>
+    </div>
+  );
+};
 
 export default App;
